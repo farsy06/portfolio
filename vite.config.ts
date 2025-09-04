3bda083931df819ecd93292950daa020051addfa
farsy06 <111578621+farsy06@users.noreply.github.com>
2025-09-04 11:11:31 +0700

    Add laravel vapor.

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,46 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    esbuild: {
-        jsx: 'automatic',
-    },
-    resolve: {
-        alias: {
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.tsx'],
+                ssr: 'resources/js/ssr.tsx',
+                refresh: true,
+            }),
+            react(),
+            tailwindcss(),
+        ],
+        esbuild: {
+            jsx: 'automatic',
         },
-    },
+        resolve: {
+            alias: {
+                'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            },
+        },
+        build: {
+            outDir: 'public/build',
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'assets/[name].[hash].js',
+                    chunkFileNames: 'assets/[name].[hash].js',
+                    assetFileNames: 'assets/[name].[hash][extname]'
+                }
+            }
+        },
+        server: {
+            hmr: {
+                host: 'localhost',
+            },
+        },
+        define: {
+            'process.env': env
+        }
+    };
 });
